99f3e37a7aa59e0fb2a2e37133eee7101cc45ddd
kavyasriramancha <ramanchakavyasri@gmail.com>
2026-03-25 11:35:40 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,49 @@
+// hello();
+// function hello()
+// {
+//     console.log("HELLO");
+// }
+
+
+// function person(name)
+// {
+//     console.log("Hello Good Afternoon " + name);
+// }
+// person("KAVYA");
+
+
+// function add(a,b){
+//     return a - b;
+// }
+// let result = add(6,2);
+// console.log(result);
+
+
+// function greet(name, callback) {
+//     console.log('Hello, ${name}!');
+//     console.log(callback());
+// }
+
+
+// function myFunction()
+// {
+//     let myName = "KAVYA";
+//     let text = "Inside: " + typeof myName + " " + myName;
+//     console.log(text);
+// }
+// myFunction();
+
+// function mydemo(x, y = 10)
+// {
+//     return x + y;
+// }
+// console.log("Shows default values " + mydemo(5));
+
+
+// const z = function(a,b) {return a*b};
+// console.log(z)
+
+hey()
+const hey = function(){
+    alert("Hey!");
+}
