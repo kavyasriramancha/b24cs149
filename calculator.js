0537995ff74e5811857f209d05a40e3d1bca0f30
kavyasriramancha <ramanchakavyasri@gmail.com>
2026-03-25 11:34:20 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,12 @@
+const PI = 3.14159; 
+function add(a, b) { 
+    return a + b; 
+} 
+function subtract(a, b) { 
+    return a - b; 
+} 
+module.exports = { 
+    add, 
+    subtract, 
+    constantPI: PI 
+}; 
